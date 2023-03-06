904367247af2a9356154f80ebe075a71b77f24de
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2023-03-06 07:04:13 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,56 +1,40 @@
-import random
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
 
-# Set the number of customers and products
+# Define variables
 num_customers = 1000
 num_products = 100
-
-# Set the date range for the data
-start_date = datetime(2017, 1, 1)
-end_date = datetime(2021, 12, 31)
-num_days = (end_date - start_date).days
-
-# Set the price range for the products
 min_price = 100
 max_price = 4000
+profit_margin_min = 0.05
+profit_margin_max = 0.17
+years = 5
+num_data_points = 1000000
 
-# Generate customer data
-customers = pd.DataFrame({
-    'customer_id': [i+1 for i in range(num_customers)],
-    'country': np.random.choice(['USA', 'UK', 'Canada', 'Australia', 'Germany'], num_customers),
-})
+# Define function to generate random profits
+def get_random_profit():
+    return np.random.uniform(profit_margin_min, profit_margin_max)
 
-# Generate product data
-products = pd.DataFrame({
-    'product_id': [i+1 for i in range(num_products)],
-    'product_price': np.random.randint(min_price, max_price+1, num_products),
-    'product_origin': np.random.choice(['USA', 'UK', 'China', 'Italy', 'Mexico'], num_products),
-})
+# Define function to generate sales data
+def generate_sales_data():
+    data = []
+    for i in range(num_data_points):
+        # Generate random data
+        customer_id = np.random.randint(1, num_customers+1)
+        product_id = np.random.randint(1, num_products+1)
+        price = np.random.randint(min_price, max_price+1)
+        quantity = np.random.randint(1, 7)
+        rating = np.random.randint(1, 5)
+        origin_country = np.random.choice(['USA', 'China', 'India', 'Japan', 'Germany'])
+        # Calculate cost and profit
+        cost = price * (1 - get_random_profit())
+        profit = price - cost
+        # Append to data
+        data.append([i+1, customer_id, product_id, price, quantity, rating, origin_country, cost, profit])
+    return pd.DataFrame(data, columns=['Sale ID', 'Customer ID', 'Product ID', 'Price', 'Quantity', 'Rating', 'Origin Country', 'Cost', 'Profit'])
 
 # Generate sales data
-sales = pd.DataFrame()
-for i in range(num_days):
-    date = start_date + timedelta(days=i)
-    num_sales = np.random.randint(0, 10)
-    if num_sales > 0:
-        customer_ids = np.random.choice(customers['customer_id'], num_sales)
-        product_ids = np.random.choice(products['product_id'], num_sales)
-        product_prices = products.loc[products['product_id'].isin(product_ids), 'product_price'].tolist()
-        product_origins = products.loc[products['product_id'].isin(product_ids), 'product_origin'].tolist()
-        sale_amounts = np.random.randint(1, 6, num_sales)
-        sale_ratings = np.random.randint(1, 6, num_sales)
-        sales_data = {
-            'date': [date for _ in range(num_sales)],
-            'customer_id': customer_ids,
-            'product_id': product_ids,
-            'product_price': product_prices,
-            'product_origin': product_origins,
-            'sale_amount': sale_amounts,
-            'sale_rating': sale_ratings,
-        }
-        sales = sales.append(pd.DataFrame(sales_data))
+sales_data = generate_sales_data()
 
-# Save the data to a CSV file
-sales.to_csv('sales_data.csv', index=False)
+# Save data to CSV file
+sales_data.to_csv('sales_data.csv', index=False)
